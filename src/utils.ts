11034937f35c6c8af51fc7e40b69cf6c502479f9
miralsuthar <miralsuthar2000@gmail.com>
2023-03-29 08:49:42 +0530

    fix: remove snapshot library initilization causing error memory out of heap

--- src/utils.ts
+++ src/utils.ts
@@ -6,11 +6,11 @@ const SNAPSHOT_ENDPOINT = `https://hub.snapshot.org/graphql`;
 
 export const ProposalBaseUrl = "https://snapshot.org/#/";
 
-const hub = "https://hub.snapshot.org";
-const client = new snapshot.Client712(hub);
+// const hub = "https://hub.snapshot.org";
+// const client = new snapshot.Client712(hub);
 
-const provide = new JsonRpcProvider("https://mainnet.infura.io/v3/cf55dddaff4d43e6992bc8a49bf097cc");
-const signer = provide.getSigner();
+// const provide = new JsonRpcProvider("https://mainnet.infura.io/v3/cf55dddaff4d43e6992bc8a49bf097cc");
+// const signer = provide.getSigner();
 
 // export const castVote = async() => {
 //   const receipt = await client.vote(signer, address, {
